e3e14b882f55f887baa213f160f419029b4b12bc
flavianventura <flavianventura@gmail.com>
2019-09-01 18:33:56 -0300

    craindo arquivos com multiploas opçãos e reduzindo a qtde de código com Cucumber

--- prato-do-dia/features/qual_o_prato_do_dia.feature
+++ prato-do-dia/features/qual_o_prato_do_dia.feature
@@ -3,9 +3,17 @@
 Funcionalidade: Qual o prato do dia
     Queremos saber qual o prato do dia na capital Paulista
 
-    Cenario: hoje é dia de Virado a Paulista
+        Esquema do Cenário: Prato do dia
 
-        Dado que hoje é segunda-feira
-        Quando eu perguntar qual é o prato do dia
-        Então a resposta deve ser "Virado a Paulista"
+            Dado que hoje é <dia>
+            Quando eu perguntar qual é o prato do dia
+            Então a resposta deve ser <resposta>
 
+            Exemplos: 
+            | dia             | resposta           |
+            | "segunda-feira" | "Virado a Paulista"|
+            | "terça-feira"   | "Dobradinha"       |
+            | "quarta-feira"  | "Feijoada"         |
+            | "quinta-feira"  | "Macarronada"      |
+            | "sexta-feira"   | "Filé de Merluza"  |
+            
--- prato-do-dia/features/step_definitions/steps.rb
+++ prato-do-dia/features/step_definitions/steps.rb
@@ -1,22 +1,30 @@
-
+# frozen_string_literal: true
 
 module Enjoeat
   def prato_do_dia(dia)
     if dia == 'segunda-feira'
-        'Virado a Paulista'
+      'Virado a Paulista'
+    elsif dia == 'terça-feira'
+      'Dobradinha'
+    elsif dia == 'quarta-feira'
+      'Feijoada'
+    elsif dia == 'quinta-feira'
+      'Macarronada'
+    elsif dia == 'sexta-feira'
+      'Filé de Merluza'
     end
   end
 end
 World Enjoeat
 
-Dado("que hoje é segunda-feira") do
-  @hoje = 'segunda-feira'
+Dado("que hoje é {string}") do |dia|
+  @hoje = dia
 end
 
 Quando("eu perguntar qual é o prato do dia") do
-  @resposta = prato_do_dia(@hoje)
+  @resposta_obtida = prato_do_dia(@hoje)
 end
 
 Então("a resposta deve ser {string}") do |resposta_esperada|
-  expect(@resposta).to eql resposta_esperada
+  expect(@resposta_obtida).to eql resposta_esperada
 end
